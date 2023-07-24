6e85829ecdbc50f94e5494b91408922380998955
ChrisBinderKub <christian.binder@kubeops.net>
2023-07-24 14:40:04 +0200

    added sudo to copy.sh

--- copy.sh
+++ copy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # export BUILDFOLDER=
 # export JDKFOLDER=
-cp -r bin/ $BUILDFOLDER
-cp -r config/ $BUILDFOLDER
-cp -r CONTRIBUTORS $BUILDFOLDER
-cp -r data/ $BUILDFOLDER
-cp -r Gemfile $BUILDFOLDER
-cp -r Gemfile.lock $BUILDFOLDER
-cp -r $JDKFOLDER/jdk/ $BUILDFOLDER
-cp -r $JDKFOLDER/JDK_VERSION $BUILDFOLDER
-cp -r lib/ $BUILDFOLDER
-cp -r LICENSE.txt $BUILDFOLDER
-cp -r logstash-core $BUILDFOLDER
-cp -r logstash-core-plugin-api/ $BUILDFOLDER
-cp -r modules/ $BUILDFOLDER
-cp -r NOTICE.TXT $BUILDFOLDER
-cp -r tools/ $BUILDFOLDER
-cp -r vendor/ $BUILDFOLDER
-cp -r x-pack/ $BUILDFOLDER
+sudo cp -r bin/ $BUILDFOLDER
+sudo cp -r config/ $BUILDFOLDER
+sudo cp -r CONTRIBUTORS $BUILDFOLDER
+sudo cp -r data/ $BUILDFOLDER
+sudo cp -r Gemfile $BUILDFOLDER
+sudo cp -r Gemfile.lock $BUILDFOLDER
+sudo cp -r $JDKFOLDER/jdk/ $BUILDFOLDER
+sudo cp -r $JDKFOLDER/JDK_VERSION $BUILDFOLDER
+sudo cp -r lib/ $BUILDFOLDER
+sudo cp -r LICENSE.txt $BUILDFOLDER
+sudo cp -r logstash-core $BUILDFOLDER
+sudo cp -r logstash-core-plugin-api/ $BUILDFOLDER
+sudo cp -r modules/ $BUILDFOLDER
+sudo cp -r NOTICE.TXT $BUILDFOLDER
+sudo cp -r tools/ $BUILDFOLDER
+sudo cp -r vendor/ $BUILDFOLDER
+sudo cp -r x-pack/ $BUILDFOLDER
